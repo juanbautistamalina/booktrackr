c80e314f7efbed02351462726479bcf43374b816
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-10-16 16:59:27 -0300

    feat: Añadir funcionalidad de filtrado de libros y mejorar la estructura de archivos
    - Se ha implementado un nuevo script `filters.js` para gestionar el filtrado de libros por estado.
    - Se han actualizado los enlaces en `books.html` para incluir atributos `data-filter` en los elementos de filtrado.
    - Se han realizado mejoras en los estilos de `style.css` para ocultar libros filtrados.
    - Se ha añadido la importación de `filters.js` en `main.js` para integrar la nueva funcionalidad.

--- 01-javascript/js/filters.js
+++ 01-javascript/js/filters.js
@@ -1,30 +1,18 @@
 const filtros = document.querySelectorAll(".filter-item");
+
 filtros.forEach((filtro) => {
   filtro.addEventListener("click", () => {
-    const filtroSeleccionado = filtro.textContent;
+    const filtroSeleccionado = filtro.getAttribute("data-filter");
+    const libros = document.querySelectorAll(".book-card");
 
-    // Actualizar los libros (en caso de que el usuario haya agregado más)
-    const libros = contenedorLibros.querySelectorAll(".book-card");
+    libros.forEach((libro) => {
+      const estado = libro.querySelector("p:last-child").getAttribute("value");
 
-    switch (filtroSeleccionado) {
-      case "Todos":
-        libros.forEach((libro) => libro.classList.remove("reading", "read", "pending"));
-        break;
-
-      case "Leyendo":
-        libros.forEach((libro) => libro.classList.remove("read", "pending"));
-        libros.forEach((libro) => libro.classList.add("reading"));
-        break;
-
-      case "Leídos":
-        libros.forEach((libro) => libro.classList.remove("reading", "pending"));
-        libros.forEach((libro) => libro.classList.add("read"));
-        break;
-
-      case "Pendientes":
-        libros.forEach((libro) => libro.classList.remove("reading", "read"));
-        libros.forEach((libro) => libro.classList.add("pending"));
-        break;
-    }
+      if (estado === filtroSeleccionado || filtroSeleccionado === "all") {
+        libro.classList.remove("hidden");
+      } else {
+        libro.classList.add("hidden");
+      }
+    });
   });
 });
--- 01-javascript/main.js
+++ 01-javascript/main.js
@@ -1,3 +1,4 @@
-import "./js/fetch-data.js"
-import "./js/modal.js"
-import "./js/avatar-element.js"
+import "./js/fetch-data.js";
+import "./js/modal.js";
+import "./js/avatar-element.js";
+import "./js/filters.js";
