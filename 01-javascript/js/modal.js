10792bfe3ba3e3a176d28eb40a07358248a4d1ef
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-10-17 12:26:27 -0300

    feat: Añadir funcionalidad de edición en el modal de libros
    - Se ha implementado un botón de guardar en el modal que permite editar el título, autor y estado de un libro seleccionado.
    - Se han añadido comentarios explicativos en el código para mejorar la comprensión de la funcionalidad.

--- 01-javascript/js/modal.js
+++ 01-javascript/js/modal.js
@@ -3,19 +3,27 @@ const modal = document.getElementById('modal');
 const modalTitle = modal.querySelector('#modal-title');
 const modalAuthor = modal.querySelector('#modal-author');
 const modalStatus = modal.querySelector('#modal-status');
+const btnSave = modal.querySelector('button');
 
 contenedorLibros.addEventListener("click", (event) => {
   const card = event.target.closest(".book-card");
-  if (card) {
-    
-    modal.classList.remove('hidden');
-    
-
+  
+  if (card) {  
+    modal.classList.remove('hidden'); // mostrar el modal
+  
     // Obtener el titulo, autor y estado del libro y mostrarlos en el modal
     modalTitle.value = card.querySelector('.book-title').textContent;
     modalAuthor.value = card.querySelector('.book-author').textContent;
     modalStatus.value = card.querySelector('.book-status').textContent;
     
+    // Editar las propiedades del libro
+    btnSave.addEventListener('click', (event) => {
+      event.preventDefault()
+      card.querySelector('.book-title').textContent = modalTitle.value;
+      card.querySelector('.book-author').textContent = modalAuthor.value;
+      card.querySelector('.book-status').textContent = modalStatus.value;
+      modal.classList.add('hidden');
+    });    
   }
 })
 
