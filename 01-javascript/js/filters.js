9cae1baeb60b161142a0d1502d7146554207b262
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-11-30 15:03:01 -0300

    refactor: optimizar la lógica de filtrado y búsqueda de libros

--- 01-javascript/js/filters.js
+++ 01-javascript/js/filters.js
@@ -1,25 +1,18 @@
-const searchBar = document.querySelector("#search-bar");
-
 // Aplicando delegación de eventos para los filtros
 const contenedorFiltros = document.querySelector(".filter-list");
 contenedorFiltros.addEventListener("click", (event) => {
-
   event.preventDefault();
-  searchBar.value = ""; // limpiar la barra de búsqueda al aplicar un filtro
+  document.querySelector("#search-bar").value = ""; // limpiar la barra de búsqueda al aplicar un filtro
 
+  // verficar si el elemento clickeado es un filtro
   if (event.target.classList.contains("filter-item")) {
     const filtroSeleccionado = event.target.getAttribute("data-filter"); // el atributo data-filter es el que contiene el estado del filtro
     const libros = document.querySelectorAll(".book-card"); // actualizar la lista de libros cada vez que se filtra
-
+    
     libros.forEach((libro) => {
       const estado = libro.querySelector("p.book-status").getAttribute("value");
-
-      // para cada libro, verificar si su estado coincide con el filtro seleccionado
-      if (estado === filtroSeleccionado || filtroSeleccionado === "all") {
-        libro.classList.remove("hidden");
-      } else {
-        libro.classList.add("hidden");
-      }
+      const isShown = filtroSeleccionado === "all" || filtroSeleccionado === estado;
+      libro.classList.toggle("hidden", !isShown);
     });
   }
 });
--- 01-javascript/js/search-bar.js
+++ 01-javascript/js/search-bar.js
@@ -1,19 +1,14 @@
 const searchBar = document.querySelector("#search-bar");
-
 searchBar.addEventListener("input", () => {
   let query = searchBar.value.toLowerCase(); // lo que el usuario escribe en la barra de búsqueda
-
   let books = document.querySelectorAll(".book-card"); // actualizar la lista de libros cada vez que el usuario escribe
 
   books.forEach((book) => {
-    let title = book.querySelector(".book-title").innerText.toLowerCase();
-    let author = book.querySelector(".book-author").innerText.toLowerCase();
+    const title = book.querySelector(".book-title").innerText.toLowerCase();
+    const author = book.querySelector(".book-author").innerText.toLowerCase();
+    const isShown = title.includes(query) || author.includes(query);
 
     // Verificar si el título o el autor incluyen la consulta de búsqueda
-    if (title.includes(query) || author.includes(query)) {
-      book.classList.remove("hidden");
-    } else {
-      book.classList.add("hidden");
-    }
+    book.classList.toggle("hidden", !isShown);
   });
 });
