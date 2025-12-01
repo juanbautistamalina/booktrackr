ad96c0eae54b00ed183b0ee9d8827004014741d5
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-01 15:32:11 -0300

    chore: eliminar carpeta de pruebas

--- 01-javascript-tests/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburgerButton = document.querySelector(".hamburger-button");
-  const mobileNav = document.querySelector(".mobile-nav");
-
-  hamburgerButton.addEventListener("click", () => {
-    hamburgerButton.classList.toggle("active");
-    mobileNav.classList.toggle("active");
-  });
-});
--- 01-javascript-tests/main.js
+++
@@ -1,4 +0,0 @@
-import "./js/fetch-data.js";
-import "./js/modal.js";
-import "./js/avatar-element.js";
-import "./js/filters.js";
