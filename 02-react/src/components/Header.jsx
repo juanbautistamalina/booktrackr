e957c4babc2dcfce066074b1c515fb45119a5acc
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-07 19:19:55 -0300

    refactor: mejorar la navegación en el componente Header

--- 02-react/src/components/Header.jsx
+++ 02-react/src/components/Header.jsx
@@ -3,10 +3,10 @@ import Avatar from "./Avatar.jsx"
 export default function Header() {
     return (
         <header className="navbar index">
-            <div className="navbar-logo">
+            <a href="index.html" class="navbar-logo">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                    className="icon icon-tabler icon-tabler-book">
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="icon icon-tabler icon-tabler-book">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
                     <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
@@ -15,7 +15,7 @@ export default function Header() {
                     <path d="M21 6l0 13" />
                 </svg>
                 <h2>BookTrackr</h2>
-            </div>
+            </a>
 
             <nav className="navbar-links desktop-nav">
                 <ul>
@@ -34,8 +34,8 @@ export default function Header() {
             </button>
 
             <div className="mobile-nav">
-                <a href="index.html" className="active">Inicio</a>
-                <a href="./pages/books.html">Mi Biblioteca</a>
+                <a href="index.html">Inicio</a>
+                <a href="./pages/books.html" className="active">Mi Biblioteca</a>
                 <a href="./pages/login.html">Iniciar Sesión</a>
             </div>
         </header>
