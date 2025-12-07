079c2dbbada585183b73573c57c46a09073b84a5
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-07 19:20:15 -0300

    feat: añadir componentes de Login y página 404 al componente App

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -2,13 +2,18 @@ import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
 import Home from './pages/Home.jsx'
 import Books from './pages/Books.jsx'
+import Login from "./pages/Login.jsx"
+import NotFoundPage from "./pages/404.jsx"
 
 
 function App() {
   return (
     <>
       <Header />
+      {/* <NotFoundPage /> */}
+      {/* <Home /> */}
       <Books />
+      {/* <Login /> */}
       <Footer />
     </>
   )
