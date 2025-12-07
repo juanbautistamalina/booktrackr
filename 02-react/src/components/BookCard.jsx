90f83d5288cf70548c1c3dc26a081dc1137bafcf
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-07 19:20:07 -0300

    feat: mejorar la visualización del estado del libro en el componente BookCard

--- 02-react/src/components/BookCard.jsx
+++ 02-react/src/components/BookCard.jsx
@@ -1,10 +1,14 @@
 export default function BookCard({ img, titulo, autor, estado, notas, genero }) {
+
+    const statusText = estado === "reading" ? "Leyendo" :
+        estado === "read" ? "Leído" : "Pendiente";
+
     return (
         <article className="book-card" data-genre={genero} data-status={estado}>
             <img src={img} alt={`Portada del libro ${titulo}`} />
             <h2 className="book-title">{titulo}</h2>
             <p className="book-author">{autor}</p>
-            <p className="book-status" value={estado}>{estado}</p>
+            <p className="book-status">{statusText}</p>
             <p className="book-notes hidden">{notas}</p>
         </article>
     )
