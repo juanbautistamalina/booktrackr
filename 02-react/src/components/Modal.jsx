627356eebc44f20910127446cf3797ebc842fa2d
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-08 20:42:29 -0300

    feat: mejorar el diseño y funcionalidad del modal, incluyendo ajustes en estilos y estructura del formulario

--- 02-react/src/components/Modal.jsx
+++ 02-react/src/components/Modal.jsx
@@ -3,40 +3,45 @@ export default function Modal() {
         <div id="modal" className='hidden'>
             <div className="modal-content">
                 <span className="close">&times;</span>
-                <h2>Editar Detalles del Libro</h2>
-                <form>
-                    <fieldset>
-                        <label htmlFor="modal-title">Titulo</label>
-                        <input type="text" id="modal-title" />
-                    </fieldset>
 
-                    <fieldset>
-                        <label htmlFor="modal-author">Autor</label>
-                        <input type="text" id="modal-author" />
-                    </fieldset>
-
-                    <div className="group-inline">
+                <div className="modal-body">
+                    <img src="/books/crimen-y-castigo.jpg" alt="" />
+                    <form>
+                        <h2>Editar Detalles del Libro</h2>
                         <fieldset>
-                            <label htmlFor="modal-status">Estado</label>
-                            <select name="" id="modal-status">
-                                <option value="reading">Leyendo</option>
-                                <option value="read">Leido</option>
-                                <option value="pending">Pendiente</option>
-                            </select>
+                            <label htmlFor="modal-title">Titulo</label>
+                            <input type="text" id="modal-title" />
                         </fieldset>
 
                         <fieldset>
-                            <label htmlFor="modal-genre">Género</label>
-                            <input type="text" id="modal-genre" />
+                            <label htmlFor="modal-author">Autor</label>
+                            <input type="text" id="modal-author" />
                         </fieldset>
-                    </div>
 
-                    <fieldset>
-                        <label htmlFor="modal-notes">Notas Personales</label>
-                        <textarea name="" id="modal-notes"></textarea>
-                    </fieldset>
-                    <button type="submit">Guardar</button>
-                </form>
+                        <div className="group-inline">
+                            <fieldset>
+                                <label htmlFor="modal-status">Estado</label>
+                                <select name="" id="modal-status">
+                                    <option value="reading">Leyendo</option>
+                                    <option value="read">Leido</option>
+                                    <option value="pending">Pendiente</option>
+                                </select>
+                            </fieldset>
+
+                            <fieldset>
+                                <label htmlFor="modal-genre">Género</label>
+                                <input type="text" id="modal-genre" />
+                            </fieldset>
+                        </div>
+
+                        <fieldset>
+                            <label htmlFor="modal-notes">Notas Personales</label>
+                            <textarea name="" id="modal-notes"></textarea>
+                        </fieldset>
+                        <button type="submit">Guardar</button>
+                    </form>
+                </div>
+
             </div>
         </div>
     )
