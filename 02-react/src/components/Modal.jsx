b23f7be7e0c65bf321c3f859c8a247c91d15122c
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-08 14:14:28 -0300

    feat: migrar modal a su propio componente

---
+++ 02-react/src/components/Modal.jsx
@@ -0,0 +1,43 @@
+export default function Modal() {
+    return (
+        <div id="modal" className='hidden'>
+            <div className="modal-content">
+                <span className="close">&times;</span>
+                <h2>Editar Detalles del Libro</h2>
+                <form>
+                    <fieldset>
+                        <label htmlFor="modal-title">Titulo</label>
+                        <input type="text" id="modal-title" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label htmlFor="modal-author">Autor</label>
+                        <input type="text" id="modal-author" />
+                    </fieldset>
+
+                    <div className="group-inline">
+                        <fieldset>
+                            <label htmlFor="modal-status">Estado</label>
+                            <select name="" id="modal-status">
+                                <option value="reading">Leyendo</option>
+                                <option value="read">Leido</option>
+                                <option value="pending">Pendiente</option>
+                            </select>
+                        </fieldset>
+
+                        <fieldset>
+                            <label htmlFor="modal-genre">Género</label>
+                            <input type="text" id="modal-genre" />
+                        </fieldset>
+                    </div>
+
+                    <fieldset>
+                        <label htmlFor="modal-notes">Notas Personales</label>
+                        <textarea name="" id="modal-notes"></textarea>
+                    </fieldset>
+                    <button type="submit">Guardar</button>
+                </form>
+            </div>
+        </div>
+    )
+}
