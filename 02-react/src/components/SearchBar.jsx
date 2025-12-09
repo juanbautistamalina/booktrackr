0d7ade06e67218c1104f99bf9a2fbdc1091fca27
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-09 10:34:58 -0300

    feat: actualizar SearchBar para manejar cambios de entrada y filtrar libros por título o autor

--- 02-react/src/components/SearchBar.jsx
+++ 02-react/src/components/SearchBar.jsx
@@ -1,6 +1,19 @@
-export default function SearchBar() {
+export default function SearchBar({ data, setFilteredBooks, searchQuery, setSearchQuery }) {
+    
+    const handleInputChange = (event) => {
+        const query = event.target.value.toLowerCase();
+        setSearchQuery(event.target.value);
+        setFilteredBooks(data.filter(book => 
+            book.title.toLowerCase().includes(query) || 
+            book.author.toLowerCase().includes(query)
+        ))
+
+    }
+    
     return (
         <input
+            onChange={handleInputChange}
+            value={searchQuery}
             className="search-bar"
             type="text"
             placeholder="Buscar libro por título o autor"
