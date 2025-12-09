7d0b0f19c15aa8658ac7e202d4bbf114b9cb8d8f
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-09 10:35:38 -0300

    feat: agregar funcionalidad de clic en BookCard para abrir modal desde BookList

--- 02-react/src/components/BookCard.jsx
+++ 02-react/src/components/BookCard.jsx
@@ -1,11 +1,11 @@
-export default function BookCard({ img, title, author, status, notes, genre }) {
+export default function BookCard({ img, title, author, status, notes, genre, onClick }) {
 
     const statusText = status === "reading" ? "Leyendo"
                      : status === "read"    ? "Leído"
                      :                        "Pendiente";
 
     return (
-        <article className="book-card" data-genre={genre} data-status={status}>
+        <article className="book-card" data-genre={genre} data-status={status} onClick={onClick}>
             <img src={img} alt={`Portada del libro ${title}`} />
             <h2 className="book-title">{title}</h2>
             <p className="book-author">{author}</p>
--- 02-react/src/components/BookList.jsx
+++ 02-react/src/components/BookList.jsx
@@ -1,11 +1,17 @@
 import BookCard from '../components/BookCard.jsx'
 
-export default function BookList({ books }) {
+export default function BookList({ books, setIsModalOpen, selectedBook, setSelectedBook }) {
+    
+    const handleClick = () => {
+        setIsModalOpen(true);
+    }
+    
     return (
         <div className="book-list">
             {books.map((book) => {
                 return (
                     <BookCard
+                        onClick={handleClick}
                         key={book.id}
                         img={book.img}
                         title={book.title}
