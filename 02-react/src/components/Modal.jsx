9308466a56ca4a3ccdebeef0073e4e01d859fd98
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-09 10:35:25 -0300

    feat: actualizar componente Modal para manejar estado y agregar botones personalizados

--- 02-react/src/components/Modal.jsx
+++ 02-react/src/components/Modal.jsx
@@ -1,8 +1,10 @@
-export default function Modal() {
+import Button from "./Button"
+
+export default function Modal({isModalOpen, setIsModalOpen}) {
     return (
-        <div id="modal" className='hidden'>
+        <div id="modal" className={isModalOpen ? 'modal-open' : 'hidden'}>
             <div className="modal-content">
-                <span className="close">&times;</span>
+                <span onClick={() => setIsModalOpen(false)} className="close">&times;</span>
 
                 <div className="modal-body">
                     <img src="/books/crimen-y-castigo.jpg" alt="" />
@@ -38,7 +40,19 @@ export default function Modal() {
                             <label htmlFor="modal-notes">Notas Personales</label>
                             <textarea name="" id="modal-notes"></textarea>
                         </fieldset>
-                        <button type="submit">Guardar</button>
+
+                        <div className="group-inline">
+
+                            <Button type="submit">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-device-floppy"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" /><path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M14 4l0 4l-6 0l0 -4" /></svg>
+                                Guardar
+                            </Button>
+
+                            <Button type="button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                                Eliminar
+                            </Button>
+                        </div>
                     </form>
                 </div>
 
