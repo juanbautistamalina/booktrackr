2229bfb3047a855d67c21a1a03a4c7db2b424d4e
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-13 19:17:37 -0300

    feat: agregar mensaje de aviso cuando no hay libros disponibles

--- 02-react/src/components/BookList.jsx
+++ 02-react/src/components/BookList.jsx
@@ -1,27 +1,34 @@
 import BookCard from '../components/BookCard.jsx'
 
 export default function BookList({ books, setIsModalOpen, setSelectedBook }) {
-    
+
     const handleClick = (book) => {
         setIsModalOpen(true);
         setSelectedBook(book);
     }
-    
+
     return (
         <div className="book-list">
-            {books.map((book) => {
-                return (
-                    <BookCard
-                        onClick={() => handleClick(book)}
-                        key={book.id}
-                        img={book.img}
-                        title={book.title}
-                        author={book.author}
-                        status={book.status}
-                        genre={book.genre}
-                        notes={book.notes} />
-                )
-            })}
+
+            {books.length === 0 ? (
+                <p>No hay libros disponibles.</p>
+            ) : (
+
+                books.map((book) => {
+                    return (
+                        <BookCard
+                            onClick={() => handleClick(book)}
+                            key={book.id}
+                            img={book.img}
+                            title={book.title}
+                            author={book.author}
+                            status={book.status}
+                            genre={book.genre}
+                            notes={book.notes} />
+                    )
+                })
+            )}
+
         </div>
     )
 }
