2bb890a062d72071e0e56d735493d9ee07b6d8c6
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-14 15:51:06 -0300

    style(Footer): migrar a CSS Modules

--- 02-react/src/components/Footer.jsx
+++ 02-react/src/components/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.css"
+
 export default function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <p>&copy; 2025 BookTrackr. Todos los derechos reservados.</p>
         </footer>
     )
