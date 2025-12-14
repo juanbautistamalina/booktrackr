ed96e0cab1e19a52093485b43fe673f97b66c82f
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-14 15:55:57 -0300

    style(Modal): migrar a CSS Modules

--- 02-react/src/components/Modal.jsx
+++ 02-react/src/components/Modal.jsx
@@ -1,27 +1,12 @@
+import styles from "./Modal.module.css"
 import Button from "./Button"
 
-export default function Modal({ dataBooks, setDataBooks, isModalOpen, setIsModalOpen, selectedBook}) {
- 
+export default function Modal({ dataBooks, setDataBooks, isModalOpen, setIsModalOpen, selectedBook }) {
+
     const { img, title, author, status, genre, notes } = selectedBook;
-    
+
     // Agregar funcionalidad para editar los campos del libro en el modal (usando los states)
     const handleEdit = () => {
-        // const updatedBooks = dataBooks.map(book => {
-        //     if (book.id === selectedBook.id) {
-        //         return {
-        //             // actualizar el listado de libros con setDataBooks...
-        //             ...book,
-        //             title: document.getElementsByClassName("modal-title").value,
-        //             author: document.getElementsByClassName("modal-author").value,
-        //             status: document.getElementsByClassName("modal-status").value,
-        //             genre: document.getElementsByClassName("modal-genre").value,
-        //             notes: document.getElementsByClassName("modal-notes").value
-        //         }
-        //     } else {
-        //         return book;
-        //     }
-        // })
-
         // setDataBooks(updatedBooks);
         setIsModalOpen(false);
     }
@@ -29,11 +14,12 @@ export default function Modal({ dataBooks, setDataBooks, isModalOpen, setIsModal
 
 
     return (
-        <div id="modal" className={isModalOpen ? 'modal-open' : 'hidden'}>
-            <div className="modal-content">
-                <span onClick={() => setIsModalOpen(false)} className="close">&times;</span>
+        <div className={`${styles.modal} ${isModalOpen ? styles.modalOpen : styles.hidden}`}>
 
-                <div className="modal-body">
+            <div className={styles.modalContent}>
+                <span onClick={() => setIsModalOpen(false)} className={styles.close}>&times;</span>
+
+                <div className={styles.modalBody}>
                     <img src={img} alt={`Portada del libro ${title}`} />
                     <form>
                         <h2>Editar Detalles del Libro</h2>
@@ -47,7 +33,7 @@ export default function Modal({ dataBooks, setDataBooks, isModalOpen, setIsModal
                             <input type="text" id="modal-author" defaultValue={author} />
                         </fieldset>
 
-                        <div className="group-inline">
+                        <div className={styles.groupInline}>
                             <fieldset>
                                 <label htmlFor="modal-status">Estado</label>
                                 <select name="" id="modal-status" defaultValue={status}>
@@ -87,7 +73,7 @@ export default function Modal({ dataBooks, setDataBooks, isModalOpen, setIsModal
                             <textarea name="" id="modal-notes" defaultValue={notes}></textarea>
                         </fieldset>
 
-                        <div className="group-inline">
+                        <div className={styles.groupInline}>
 
                             <Button type="button" onClick={handleEdit}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-device-floppy"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" /><path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M14 4l0 4l-6 0l0 -4" /></svg>
