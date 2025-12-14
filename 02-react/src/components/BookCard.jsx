f9c23565b9e7acbe20e1e9b7e804693e184d449a
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-14 15:49:59 -0300

    style(BookCard): migrar a CSS Modules

--- 02-react/src/components/BookCard.jsx
+++ 02-react/src/components/BookCard.jsx
@@ -1,3 +1,5 @@
+import styles from "./BookCard.module.css"
+
 export default function BookCard({ img, title, author, status, notes, genre, onClick }) {
 
     const statusText = status === "reading" ? "Leyendo"
@@ -5,12 +7,12 @@ export default function BookCard({ img, title, author, status, notes, genre, onC
                      :                        "Pendiente";
 
     return (
-        <article className="book-card" data-genre={genre} data-status={status} onClick={onClick}>
+        <article className={styles.bookCard} data-genre={genre} data-status={status} onClick={onClick}>
             <img src={img} alt={`Portada del libro ${title}`} />
-            <h2 className="book-title">{title}</h2>
-            <p className="book-author">{author}</p>
-            <p className="book-status">{statusText}</p>
-            <p className="book-notes hidden">{notes}</p>
+            <h2>{title}</h2>
+            <p className={styles.bookAuthor}>{author}</p>
+            <p className={styles.bookStatus}>{statusText}</p>
+            <p className={styles.bookNotes}>{notes}</p>
         </article>
     )
 }
