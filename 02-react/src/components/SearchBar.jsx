68ed81df0028828fb808e2a6e0e820134dbaf75c
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-15 15:57:33 -0300

    refactor(SearchBar): simplificar props y eliminar lógica de filtrado no usada

--- 02-react/src/components/SearchBar.jsx
+++ 02-react/src/components/SearchBar.jsx
@@ -1,15 +1,9 @@
 import styles from "./SearchBar.module.css"
 
-export default function SearchBar({ data, setFilteredBooks, searchQuery, setSearchQuery }) {
+export default function SearchBar({ searchQuery, setSearchQuery }) {
     
     const handleInputChange = (event) => {
-        const query = event.target.value.toLowerCase();
         setSearchQuery(event.target.value);
-        setFilteredBooks(data.filter(book => 
-            book.title.toLowerCase().includes(query) || 
-            book.author.toLowerCase().includes(query)
-        ))
-
     }
     
     return (
