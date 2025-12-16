51e1aa7a8e94a435ea37a7339ff1b9af24ee530c
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-16 16:01:12 -0300

    refactor(Modal): añadir funciones para editar y eliminar libros

--- 02-react/src/components/Modal.jsx
+++ 02-react/src/components/Modal.jsx
@@ -1,11 +1,50 @@
 import styles from "./Modal.module.css"
 import Button from "./Button"
+import { useId } from "react";
 
-export default function Modal({ isModalOpen, setIsModalOpen, selectedBook }) {
+export default function Modal({ setBooks, isModalOpen, setIsModalOpen, selectedBook }) {
+
+    const idTitle = useId();
+    const idAuthor = useId();
+    const idStatus = useId();
+    const idGenre = useId();
+    const idNotes = useId();
 
     if (!isModalOpen || !selectedBook) return null;
 
     const { img, title, author, status, genre, notes } = selectedBook;
+
+    const handleEdit = (event) => {
+        event.preventDefault();
+
+        const modalData = new FormData(event.target);
+
+        const updatedBook = {
+            title: modalData.get("modal-title"),
+            author: modalData.get("modal-author"),
+            status: modalData.get("modal-status"),
+            genre: modalData.get("modal-genre"),
+            notes: modalData.get("modal-notes")
+        }
+
+        // Actualiza las propiedades del libro editado y no modifica el resto de libros
+        setBooks(prevBooks => {
+            return prevBooks.map(book =>
+                book.id === selectedBook.id
+                    ? { ...book, ...updatedBook }
+                    : book
+            )
+        });
+
+        setIsModalOpen(false);
+
+    }
+
+    const handleDelete = () => {
+        setBooks(prevBooks => prevBooks.filter(book => book.id !== selectedBook.id));
+        setIsModalOpen(false);
+    }
+
 
     return (
         <div className={`${styles.modal} ${isModalOpen ? styles.modalOpen : styles.hidden}`}>
@@ -15,22 +54,22 @@ export default function Modal({ isModalOpen, setIsModalOpen, selectedBook }) {
 
                 <div className={styles.modalBody}>
                     <img src={img} alt={`Portada del libro ${title}`} />
-                    <form>
+                    <form onSubmit={handleEdit}>
                         <h2>Editar Detalles del Libro</h2>
                         <fieldset>
-                            <label htmlFor="modal-title">Titulo</label>
-                            <input type="text" id="modal-title" defaultValue={title} />
+                            <label htmlFor={idTitle}>Titulo</label>
+                            <input type="text" name="modal-title" id={idTitle} defaultValue={title} />
                         </fieldset>
 
                         <fieldset>
-                            <label htmlFor="modal-author">Autor</label>
-                            <input type="text" id="modal-author" defaultValue={author} />
+                            <label htmlFor={idAuthor}>Autor</label>
+                            <input type="text" name="modal-author" id={idAuthor} defaultValue={author} />
                         </fieldset>
 
                         <div className={styles.groupInline}>
                             <fieldset>
-                                <label htmlFor="modal-status">Estado</label>
-                                <select id="modal-status" defaultValue={status}>
+                                <label htmlFor={idStatus}>Estado</label>
+                                <select name="modal-status" id={idStatus} defaultValue={status}>
                                     <option value="reading">Leyendo</option>
                                     <option value="read">Leido</option>
                                     <option value="pending">Pendiente</option>
@@ -38,8 +77,8 @@ export default function Modal({ isModalOpen, setIsModalOpen, selectedBook }) {
                             </fieldset>
 
                             <fieldset>
-                                <label htmlFor="modal-genre">Género</label>
-                                <select id="modal-genre" defaultValue={genre}>
+                                <label htmlFor={idGenre}>Género</label>
+                                <select name="modal-genre" id={idGenre} defaultValue={genre}>
                                     <option value="Novela">Novela</option>
                                     <option value="Filosofia">Filosofía</option>
                                     <option value="Autoayuda">Autoayuda</option>
@@ -54,30 +93,28 @@ export default function Modal({ isModalOpen, setIsModalOpen, selectedBook }) {
                                     <option value="Existencialismo">Existencialismo</option>
                                     <option value="Otro">Otro</option>
                                 </select>
-
                             </fieldset>
                         </div>
 
                         <fieldset>
-                            <label htmlFor="modal-notes">Notas Personales</label>
-                            <textarea id="modal-notes" defaultValue={notes}></textarea>
+                            <label htmlFor={idNotes}>Notas Personales</label>
+                            <textarea name="modal-notes" id={idNotes} defaultValue={notes}></textarea>
                         </fieldset>
 
                         <div className={styles.groupInline}>
 
-                            <Button type="button">
+                            <Button type="submit">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-device-floppy"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" /><path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M14 4l0 4l-6 0l0 -4" /></svg>
                                 Guardar
                             </Button>
 
-                            <Button type="button">
+                            <Button type="button" onClick={handleDelete}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
                                 Eliminar
                             </Button>
                         </div>
                     </form>
                 </div>
-
             </div>
         </div>
     )
