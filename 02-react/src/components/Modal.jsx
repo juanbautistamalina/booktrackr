f01e38cc52e19e989ed14423f791bbf899cfecb4
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-17 16:11:00 -0300

    refactor(Modal): mejorar la lógica de edición y adición de libros

--- 02-react/src/components/Modal.jsx
+++ 02-react/src/components/Modal.jsx
@@ -10,35 +10,48 @@ export default function Modal({ setBooks, isModalOpen, setIsModalOpen, selectedB
     const idGenre = useId();
     const idNotes = useId();
 
-    if (!isModalOpen || !selectedBook) return null;
+    if (!isModalOpen) return null;
 
-    const { img, title, author, status, genre, notes } = selectedBook;
+    const {
+        img = "",
+        title = "",
+        author = "",
+        status = "reading",
+        genre = "",
+        notes = ""
+    } = selectedBook || {};
 
-    const handleEdit = (event) => {
+
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-        const modalData = new FormData(event.target);
+        const formData = new FormData(event.target);
 
-        const updatedBook = {
-            title: modalData.get("modal-title"),
-            author: modalData.get("modal-author"),
-            status: modalData.get("modal-status"),
-            genre: modalData.get("modal-genre"),
-            notes: modalData.get("modal-notes")
-        }
+        const bookData = {
+            id: selectedBook?.id ?? crypto.randomUUID(),
+            img: selectedBook?.img ?? "/books/default.jpg",
+            title: formData.get("modal-title"),
+            author: formData.get("modal-author"),
+            status: formData.get("modal-status"),
+            genre: formData.get("modal-genre"),
+            notes: formData.get("modal-notes"),
+        };
 
-        // Actualiza las propiedades del libro editado y no modifica el resto de libros
         setBooks(prevBooks => {
-            return prevBooks.map(book =>
-                book.id === selectedBook.id
-                    ? { ...book, ...updatedBook }
-                    : book
-            )
+            if (selectedBook) {
+                // editar
+                return prevBooks.map(book =>
+                    book.id === selectedBook.id ? bookData : book
+                );
+            }
+
+            // agregar
+            return [...prevBooks, bookData];
         });
 
         setIsModalOpen(false);
+    };
 
-    }
 
     const handleDelete = () => {
         setBooks(prevBooks => prevBooks.filter(book => book.id !== selectedBook.id));
@@ -53,67 +66,76 @@ export default function Modal({ setBooks, isModalOpen, setIsModalOpen, selectedB
                 <span onClick={() => setIsModalOpen(false)} className={styles.close}>&times;</span>
 
                 <div className={styles.modalBody}>
-                    <img src={img} alt={`Portada del libro ${title}`} />
-                    <form onSubmit={handleEdit}>
-                        <h2>Editar Detalles del Libro</h2>
-                        <fieldset>
-                            <label htmlFor={idTitle}>Titulo</label>
-                            <input type="text" name="modal-title" id={idTitle} defaultValue={title} />
-                        </fieldset>
+                    <img src={img || "/books/default.jpg"} alt={`Portada del libro ${title}`} />
 
-                        <fieldset>
-                            <label htmlFor={idAuthor}>Autor</label>
-                            <input type="text" name="modal-author" id={idAuthor} defaultValue={author} />
-                        </fieldset>
+                    {/* <input type="file" id="fileInput" accept="image/*" />
 
-                        <div className={styles.groupInline}>
+                        <label for="fileInput" class="custom-file-upload">
+                            Seleccionar Archivo de Imagen
+                        </label> */}
+
+                        <form onSubmit={handleSubmit}>
+                            <h2>{selectedBook ? "Editar Detalles del Libro" : "Agregar Nuevo Libro"}</h2>
                             <fieldset>
-                                <label htmlFor={idStatus}>Estado</label>
-                                <select name="modal-status" id={idStatus} defaultValue={status}>
-                                    <option value="reading">Leyendo</option>
-                                    <option value="read">Leido</option>
-                                    <option value="pending">Pendiente</option>
-                                </select>
+                                <label htmlFor={idTitle}>Titulo</label>
+                                <input type="text" name="modal-title" id={idTitle} defaultValue={title} />
                             </fieldset>
 
                             <fieldset>
-                                <label htmlFor={idGenre}>Género</label>
-                                <select name="modal-genre" id={idGenre} defaultValue={genre}>
-                                    <option value="Novela">Novela</option>
-                                    <option value="Filosofia">Filosofía</option>
-                                    <option value="Autoayuda">Autoayuda</option>
-                                    <option value="Ensayo">Ensayo</option>
-                                    <option value="Historia">Historia</option>
-                                    <option value="Biografia">Biografía</option>
-                                    <option value="Cienciaficcion">Ciencia Ficción</option>
-                                    <option value="Fantasia">Fantasía</option>
-                                    <option value="Poesia">Poesía</option>
-                                    <option value="Espiritualidad">Espiritualidad</option>
-                                    <option value="Distopia">Distopía</option>
-                                    <option value="Existencialismo">Existencialismo</option>
-                                    <option value="Otro">Otro</option>
-                                </select>
+                                <label htmlFor={idAuthor}>Autor</label>
+                                <input type="text" name="modal-author" id={idAuthor} defaultValue={author} />
                             </fieldset>
-                        </div>
 
-                        <fieldset>
-                            <label htmlFor={idNotes}>Notas Personales</label>
-                            <textarea name="modal-notes" id={idNotes} defaultValue={notes}></textarea>
-                        </fieldset>
+                            <div className={styles.groupInline}>
+                                <fieldset>
+                                    <label htmlFor={idStatus}>Estado</label>
+                                    <select name="modal-status" id={idStatus} defaultValue={status}>
+                                        <option value="reading">Leyendo</option>
+                                        <option value="read">Leido</option>
+                                        <option value="pending">Pendiente</option>
+                                    </select>
+                                </fieldset>
 
-                        <div className={styles.groupInline}>
+                                <fieldset>
+                                    <label htmlFor={idGenre}>Género</label>
+                                    <select name="modal-genre" id={idGenre} defaultValue={genre}>
+                                        <option value="Novela">Novela</option>
+                                        <option value="Filosofia">Filosofía</option>
+                                        <option value="Autoayuda">Autoayuda</option>
+                                        <option value="Ensayo">Ensayo</option>
+                                        <option value="Historia">Historia</option>
+                                        <option value="Biografia">Biografía</option>
+                                        <option value="Cienciaficcion">Ciencia Ficción</option>
+                                        <option value="Fantasia">Fantasía</option>
+                                        <option value="Poesia">Poesía</option>
+                                        <option value="Espiritualidad">Espiritualidad</option>
+                                        <option value="Distopia">Distopía</option>
+                                        <option value="Existencialismo">Existencialismo</option>
+                                        <option value="Otro">Otro</option>
+                                    </select>
+                                </fieldset>
+                            </div>
 
-                            <Button type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-device-floppy"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" /><path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M14 4l0 4l-6 0l0 -4" /></svg>
-                                Guardar
-                            </Button>
+                            <fieldset>
+                                <label htmlFor={idNotes}>Notas Personales</label>
+                                <textarea name="modal-notes" id={idNotes} defaultValue={notes}></textarea>
+                            </fieldset>
 
-                            <Button type="button" onClick={handleDelete}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
-                                Eliminar
-                            </Button>
-                        </div>
-                    </form>
+                            <div className={styles.groupInline}>
+
+                                <Button type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-device-floppy"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" /><path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M14 4l0 4l-6 0l0 -4" /></svg>
+                                    Guardar
+                                </Button>
+
+                                {selectedBook && (
+                                    <Button type="button" onClick={handleDelete}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                                        Eliminar
+                                    </Button>
+                                )}
+                            </div>
+                        </form>
                 </div>
             </div>
         </div>
