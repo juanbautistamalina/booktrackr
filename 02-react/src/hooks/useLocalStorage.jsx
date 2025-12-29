57644621700b06d940c1bd911b173700744034fb
Juan Bautista Malina <malinajuanbautista@gmail.com>
2025-12-28 21:23:22 -0300

    refactor(useLocalStorage): mejorar la documentación y corregir el nombre de la variable para reflejar el uso de 'books'

--- 02-react/src/hooks/useLocalStorage.jsx
+++ 02-react/src/hooks/useLocalStorage.jsx
@@ -1,19 +1,24 @@
 import { useState, useEffect } from 'react'
 
 export default function useLocalStorage(key, initialValue) {
-  
-    // Obtener valor inicial del localStorage
+
+  /* 
+    Obtener valor inicial del localStorage:
+   * Si books existe en localStorage, quiere decir que ya hay libros guardados/editados
+   * Si no, usar el valor inicial (data.json) 
+  */
+ 
   const [value, setValue] = useState(() => {
     try {
-      const book = window.localStorage.getItem(key)
-      return book ? JSON.parse(book) : initialValue
+      const books = window.localStorage.getItem(key)
+      return books ? JSON.parse(books) : initialValue
     } catch (error) {
       console.error(error)
       return initialValue
     }
   })
 
-  // Actualizar localStorage cuando cambia el valor
+  // Actualizar localStorage cuando cambia el valor, es decir, cuando se agrega/edita/elimina un libro
   useEffect(() => {
     try {
       window.localStorage.setItem(key, JSON.stringify(value))
