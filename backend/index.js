8d40f502b008caec693a6cdfad4fed87fb316d68
Juan Bautista Malina <malinajuanbautista@gmail.com>
2026-04-20 16:49:57 -0300

    configure production deployment (render + supabase + vercel)

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ app.use("/api/auth", authRouter);
 
 app.use("/api/books", booksRouter);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
 });
+
